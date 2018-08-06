7b1e7c3fb68a1be92ff05f66b95ac14884fd97ea
edulop <elopez@chanzuckerberg.com>
2018-08-06 16:14:56 -0700

    ec2 poweruser

---
+++ aws-iam-role-ec2-poweruser/README.md
@@ -0,0 +1,16 @@
+<!-- START -->
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| role_name |  | string | - | yes |
+| source_account_id |  | string | - | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| arn |  |
+
+<!-- END -->
---
+++ aws-iam-role-ec2-poweruser/main.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "ec2-poweruser" {
+  name = "${var.role_name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": {
+      "AWS": "arn:aws:iam::${var.source_account_id}:root"
+    },
+    "Action": "sts:AssumeRole"
+  }
+}
+EOF
+}
+
+resource "aws_iam_policy" "ec2" {
+  name        = "${var.role_name}-ec2"
+  description = "Provides full access to the ec2 api"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "ssm",
+      "Action": [
+        "ec2:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ec2" {
+  role       = "${aws_iam_role.ec2-poweruser.name}"
+  policy_arn = "${aws_iam_policy.ec2.arn}"
+}
