dcacc32d7d840b57b1137191c664a1b1f7621c51
Ryan King <rking@chanzuckerberg.com>
2018-08-06 12:51:03 -0700

    wip

---
+++ aws-iam-role-readonly/module_test.go
@@ -0,0 +1,45 @@
+package role_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAWSIAMRoleReadOnly(t *testing.T) {
+	t.Parallel()
+
+	region := "us-west-1"
+	session, err := aws.NewAuthenticatedSession(region)
+	assert.Nil(t, err)
+	stsClient := sts.New(session)
+
+	caller, err := stsClient.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	assert.Nil(t, err)
+	curAcct := *caller.Account
+	assert.NotNil(t, curAcct)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: ".",
+
+		Vars: map[string]interface{}{
+			"role_name":         random.UniqueId(),
+			"source_account_id": curAcct,
+			"iam_path":          fmt.Sprintf("/%s/", random.UniqueId()),
+		},
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": region,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	_, err = terraform.InitAndApplyE(t, terraformOptions)
+
+	assert.Nil(t, err)
+}
