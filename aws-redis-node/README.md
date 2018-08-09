03bb6bc38d2c8299a45f2d98e8cf2e8b8466fcbe
Ryan King <ryan@theryanking.com>
2018-08-09 15:23:42 -0700

    add aws-redis-node (#27)
    Fixes #25

---
+++ aws-redis-node/README.md
@@ -0,0 +1,33 @@
+# Redis Node
+
+This module creates a single-node Elasticache Redis cluster with the given
+parameters.
+
+<!-- START -->
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| apply_immediately |  | string | `true` | no |
+| availability_zone |  | string | - | yes |
+| engine_version |  | string | `4.0.10` | no |
+| env |  | string | - | yes |
+| ingress_security_group_ids |  | list | - | yes |
+| instance_type |  | string | `cache.m4.large` | no |
+| owner |  | string | - | yes |
+| parameter_group_name |  | string | `default.redis3.2` | no |
+| port |  | string | `6379` | no |
+| project |  | string | - | yes |
+| resource_name | If not set, name will be ${var.project}-${var.env}-${var.name}. | string | `` | no |
+| service |  | string | `redis` | no |
+| subnets |  | list | - | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| address |  |
+| port |  |
+
+<!-- END -->
---
+++ aws-redis-node/main.tf
@@ -0,0 +1,32 @@
+locals {
+  name = "${var.project}-${var.env}-${var.service}"
+
+  tags = {
+    managedBy = "terraform"
+    Name      = "${var.project}-${var.env}-${var.service}"
+    project   = "${var.project}"
+    env       = "${var.env}"
+    service   = "${var.service}"
+    owner     = "${var.owner}"
+  }
+}
+
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "${var.resource_name != "" ? var.resource_name : local.name}"
+  subnet_ids = ["${var.subnets}"]
+}
+
+resource "aws_elasticache_cluster" "default" {
+  cluster_id           = "${var.resource_name != "" ? var.resource_name : local.name}"
+  engine               = "redis"
+  engine_version       = "3.2.6"
+  node_type            = "${var.instance_type}"
+  port                 = "${var.port}"
+  num_cache_nodes      = 1
+  parameter_group_name = "${var.parameter_group_name}"
+  subnet_group_name    = "${aws_elasticache_subnet_group.default.name}"
+  security_group_ids   = ["${var.ingress_security_group_ids}"]
+  apply_immediately    = "${var.apply_immediately}"
+  availability_zone    = "${var.availability_zone}"
+  tags                 = "${local.tags}"
+}
