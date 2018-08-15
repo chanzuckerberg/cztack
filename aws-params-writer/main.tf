92130adbdee90ba3b2403ac63665f50df8b3c85b
Ryan King <ryan@theryanking.com>
2018-08-15 09:50:53 -0700

    params refactor (#35)
    A little bit different than planned, we now have–
    
    aws-param for reading a single param
    aws-params-writer for writing a batch of params for a service
    aws-params-secrets-setup for doing the per-account setup for chamber compat
    aws-params-reader-policy for creating policy to read secrets.

---
+++ aws-params-writer/main.tf
@@ -0,0 +1,26 @@
+locals {
+  service_name = "${var.project}-${var.env}-${var.service}"
+}
+
+data "aws_kms_key" "key" {
+  key_id = "alias/parameter_store_key"
+}
+
+resource "aws_ssm_parameter" "parameter" {
+  count = "${length(keys(var.parameters))}"
+
+  name  = "/${local.service_name}/${element(keys(var.parameters), count.index)}"
+  value = "${lookup(var.parameters, element(keys(var.parameters), count.index))}"
+
+  type      = "SecureString"
+  key_id    = "${data.aws_kms_key.key.id}"
+  overwrite = "true"
+
+  tags = {
+    managedBy = "terraform"
+    project   = "${var.project}"
+    env       = "${var.env}"
+    service   = "${var.service}"
+    owner     = "${var.owner}"
+  }
+}
