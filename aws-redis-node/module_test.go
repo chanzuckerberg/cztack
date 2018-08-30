f6eaf7a26ca3cfdb949e3ba8875a6a71e9c7e5bd
Ryan King <rking@chanzuckerberg.com>
2018-08-30 08:52:22 -0700

    init + apply test for aws-redis-node
    This is our first test that uses one of our CI VPCs. The ids needed for this (vpc, subnets, etc) are being passed in as environment variables.
    
    My thought is that if others want to fork and run the tests in their own infra, they can just set those env variables.

--- aws-redis-node/module_test.go
+++ aws-redis-node/module_test.go
@@ -1,14 +1,52 @@
 package test
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"testing"
 
+	"github.com/chanzuckerberg/cztack/testutil"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestAWSRedisNode(t *testing.T) {
+
+	private_subnets := testutil.ListEnvVar("PRIVATE_SUBNETS")
+	log.Printf("subnets %#v\n", private_subnets)
+	log.Printf("subnets %#v\n", os.Getenv("PRIVATE_SUBNETS"))
+	vpc := testutil.EnvVar("VPC_ID")
+
+	sg := testutil.CreateSecurityGroup(t, testutil.DefaultRegion, vpc)
+	defer testutil.DeleteSecurityGroup(t, testutil.DefaultRegion, sg)
+
+	project := testutil.UniqueId()
+	env := testutil.UniqueId()
+	service := testutil.UniqueId()
+	owner := testutil.UniqueId()
+
+	az := fmt.Sprintf("%sa", testutil.DefaultRegion)
+
 	options := &terraform.Options{
 		TerraformDir: ".",
+
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": testutil.DefaultRegion,
+		},
+
+		Vars: map[string]interface{}{
+			"project": project,
+			"env":     env,
+			"service": service,
+			"owner":   owner,
+
+			"availability_zone":          az,
+			"subnets":                    private_subnets,
+			"ingress_security_group_ids": []string{sg},
+		},
 	}
-	terraform.Init(t, options)
+
+	defer testutil.Cleanup(t, options)
+
+	testutil.Run(t, options)
 }
--- aws-redis-node/variables.tf
+++ aws-redis-node/variables.tf
@@ -14,15 +14,18 @@ variable "owner" {
 }
 
 variable "subnets" {
-  type = "list"
+  type        = "list"
+  description = "List of subnets to which this EC instance should be attached. They should probably be private."
 }
 
 variable "availability_zone" {
-  type = "string"
+  type        = "string"
+  description = "Availability zone in which this instance should run."
 }
 
 variable "ingress_security_group_ids" {
-  type = "list"
+  type        = "list"
+  description = "Source security groups which should be able to contact this instance."
 }
 
 variable "service" {
@@ -37,7 +40,9 @@ variable "port" {
 }
 
 variable "instance_type" {
-  default = "cache.m4.large"
+  type        = "string"
+  description = "The type of instance to run. See [supported node types](https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/CacheNodes.SupportedTypes.html)"
+  default     = "cache.m4.large"
 }
 
 variable "parameter_group_name" {
@@ -45,11 +50,15 @@ variable "parameter_group_name" {
 }
 
 variable "engine_version" {
-  default = "4.0.10"
+  type        = "string"
+  description = "The version of Redis to run. See [supported versions](https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/supported-engine-versions.html)"
+  default     = "4.0.10"
 }
 
 variable "apply_immediately" {
-  default = true
+  type        = "string"
+  description = "Whether changes should be applied immediately or during the next maintenance window."
+  default     = true
 }
 
 # This is to get a around a limitation where the elasticache cluster id can be
