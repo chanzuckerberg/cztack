d0687f6cf3aa8a524d4b724a97e80711816104ae
Michael Barrientos <mbarrientos@chanzuckerberg.com>
2018-10-08 16:33:20 -0700

    aws-iam-instance-profile ignore name_prefix

--- aws-iam-instance-profile/main.tf
+++ aws-iam-instance-profile/main.tf
@@ -15,6 +15,10 @@ resource "aws_iam_role" "role" {
   description        = "${var.role_description}"
   path               = "${var.iam_path}"
   assume_role_policy = "${data.aws_iam_policy_document.assume-role.json}"
+
+  lifecycle {
+    ignore_changes = ["name", "name_prefix", "path"]
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "ssm" {
@@ -31,4 +35,8 @@ resource "aws_iam_instance_profile" "profile" {
   name_prefix = "${var.name_prefix}"
   path        = "${var.iam_path}"
   role        = "${aws_iam_role.role.name}"
+
+  lifecycle {
+    ignore_changes = ["name", "name_prefix", "path"]
+  }
 }
