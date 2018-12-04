58a7d255725daa5ecfd3c72c8d0a0693e6bc0995
Huzefa Fatakdawala <huzefa@users.noreply.github.com>
2018-12-04 15:10:29 -0800

    Add the reader endpoint output from the aws aurora resource.

--- aws-aurora-mysql/outputs.tf
+++ aws-aurora-mysql/outputs.tf
@@ -5,3 +5,7 @@ output "database_name" {
 output "endpoint" {
   value = "${module.aurora.endpoint}"
 }
+
+output "reader_endpoint" {
+  value = "${module.aurora.reader_endpoint}"
+}
--- aws-aurora/outputs.tf
+++ aws-aurora/outputs.tf
@@ -6,6 +6,10 @@ output "endpoint" {
   value = "${aws_rds_cluster.db.endpoint}"
 }
 
+output "reader_endpoint" {
+  value = "${aws_rds_cluster.db.reader_endpoint}"
+}
+
 output "port" {
   value = "${var.port}"
 }
