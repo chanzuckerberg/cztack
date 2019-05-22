b9a5b50ff67eebeba2655d11d9c60a9fccf2404e
Chau Vu <cvu@chanzuckerberg.com>
2019-05-22 14:28:40 -0700

    Output domain_validation_options

--- aws-acm-cert/outputs.tf
+++ aws-acm-cert/outputs.tf
@@ -5,3 +5,7 @@ output "arn" {
 output "id" {
   value = "${aws_acm_certificate.cert.id}"
 }
+
+output "domain_validation_options" {
+  value = ["${aws_acm_certificate.cert.domain_validation_options}"]
+}
