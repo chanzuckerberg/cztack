b9aa3c98300a73ed0e43baf90902927dfb822685
Chau Vu <cvu@chanzuckerberg.com>
2019-05-22 15:33:38 -0700

    Add back precomputed cert_validation_count

--- aws-acm-cert/main.tf
+++ aws-acm-cert/main.tf
@@ -24,8 +24,8 @@ resource "aws_acm_certificate" "cert" {
 
 # https://www.terraform.io/docs/providers/aws/r/acm_certificate_validation.html
 resource "aws_route53_record" "cert_validation" {
-  # count = "${local.cert_validation_count}"
-  count = "${length(aws_acm_certificate.cert.domain_validation_options)}"
+  count = "${local.cert_validation_count}"
+  # count = "${length(aws_acm_certificate.cert.domain_validation_options)}"
 
   name    = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_name")}"
   type    = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_type")}"
