ea436b63c2d3abfbf3acf262cf5f0cbda1b809f0
Ryan King <rking@chanzuckerberg.com>
2019-06-05 15:39:55 -0700

    add force_detach_policies to aws_iam_role
    This enables the role to be recreated without using name_prefix + create_before_destroy.

--- aws-iam-role-crossacct/main.tf
+++ aws-iam-role-crossacct/main.tf
@@ -13,4 +13,9 @@ resource "aws_iam_role" "role" {
   name               = "${var.role_name}"
   path               = "${var.iam_path}"
   assume_role_policy = "${data.aws_iam_policy_document.assume-role.json}"
+
+  # We have to force detach policies in order to recreate roles.
+  # The other option would be to use name_prefix and create_before_destroy, but that
+  # doesn't work if you want a role with a stable, memorable name.
+  force_detach_policies = true
 }
