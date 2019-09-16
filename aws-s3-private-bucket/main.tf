6923e1b62582134e4f19af34bf30f2b2520a016f
Michael Barrientos <mbarrien@users.noreply.github.com>
2019-09-16 12:32:27 -0700

    Support disabling versioning in private buckets (#122)

--- aws-s3-private-bucket/main.tf
+++ aws-s3-private-bucket/main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "bucket" {
   policy = "${data.aws_iam_policy_document.bucket_policy.json}"
 
   versioning {
-    enabled = true
+    enabled = var.enable_versioning
   }
 
   # TODO
--- aws-s3-private-bucket/variables.tf
+++ aws-s3-private-bucket/variables.tf
@@ -22,3 +22,9 @@ variable "service" {
 variable "owner" {
   type = "string"
 }
+
+variable "enable_versioning" {
+  type        = bool
+  description = "Keep old versions of overwritten S3 objects."
+  default     = true
+}
