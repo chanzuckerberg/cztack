2faeae6c8163c395207b6d4ecc935664d8b302b7
Michael Barrientos <mbarrien@users.noreply.github.com>
2019-09-17 10:10:14 -0700

    aws-s3-private-bucket set ignore_public_acls and restrict_public_buckets (#124)

--- aws-s3-private-bucket/main.tf
+++ aws-s3-private-bucket/main.tf
@@ -37,8 +37,10 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_public_access_block" "bucket" {
   bucket = "${aws_s3_bucket.bucket.id}"
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 data "aws_iam_policy_document" "bucket_policy" {
