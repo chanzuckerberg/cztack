3b30c7386ffa5bca433464677a43ff81b52b9e44
Annie <anne.ku@chanzuckerberg.com>
2019-09-23 12:30:52 -0700

    removed duplicates from arn list in dynamic statement

--- aws-iam-role-infraci/main.tf
+++ aws-iam-role-infraci/main.tf
@@ -66,7 +66,7 @@ data "aws_iam_policy_document" "secrets" {
 
   dynamic statement {
 
-    for_each = var.terraform_state_lock_dynamodb_arns
+    for_each = distinct(var.terraform_state_lock_dynamodb_arns)
 
 
     content {
