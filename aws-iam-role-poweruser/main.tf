61d294b57fdc12c795d995864574059b65f17172
Michael Barrientos <mbarrien@users.noreply.github.com>
2019-10-10 11:12:31 -0700

    Add UpdateRoleDescription perms to aws-iam-role-poweruser (#145)

--- aws-iam-role-poweruser/main.tf
+++ aws-iam-role-poweruser/main.tf
@@ -72,6 +72,7 @@ data "aws_iam_policy_document" "misc" {
       "iam:TagRole",
       "iam:UpdateAssumeRolePolicy",
       "iam:UpdateRole",
+      "iam:UpdateRoleDescription",
     ]
 
     resources = ["*"]
