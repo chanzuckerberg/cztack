e735b97ca2cee2d3e05121c2a9e83a1f5e1ba37e
Ryan King <rking@chanzuckerberg.com>
2020-06-24 08:53:36 -0700

    WIP attempt at making TDD more productive
    This uses terratest's test-structure tools to make it easier to iterate
    on this code.
    
    The idea is that each test will be broken into stages like 'options',
    'apply', 'validate' and 'cleanup' which can be skipped but also persist
    any state (like randomly generated options).
    
    So you can do something like:
    
    `make test TEST=./aws-s3-private-bucket SKIP_cleanup=1`
    
    Which will
    
    1. run 'options' to generate a set of random variables as input
    for this test and perist them
    2. run 'apply' which will use `terraform apply` to create resources and perist the state file
    3. run 'validate' to make assertions about the resources created
    4. skip 'cleanup'
    
    This will take 1-2min.
    
    Then if you run:
    
    `make test TEST=./aws-s3-private-bucket SKIP_cleanup=1 SKIP_options=1 SKIP_apply=1`
    
    It will only run the validate step, which takes < 10s. You can re run
    this, iterating on your tests until you are satisfied.
    
    At some point you might realize you need to update the terraform code.
    After you make those edits you can run:
    
    `make test TEST=./aws-s3-private-bucket SKIP_cleanup=1 SKIP_options=1`
    
    which will
    
    1. skip options, using the existing options
    2. run apply, using existing options and state file. This will mean that
       for some resources you can do an update-in-place, potentially much
       faster.
    3. run validate
    4. skip cleanup
    
    When all done, you can run cleanup and then re-run with no skips to make
    sure it works end-to-end.

--- aws-s3-private-bucket/module_test.go
+++ aws-s3-private-bucket/module_test.go
@@ -3,31 +3,97 @@ package test
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/chanzuckerberg/cztack/testutil"
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/require"
 )
 
-func TestPrivateBucket(t *testing.T) {
+func TestPrivateBucketDefaults(t *testing.T) {
 
-	project := testutil.UniqueId()
-	env := testutil.UniqueId()
-	service := testutil.UniqueId()
-	owner := testutil.UniqueId()
+	test := &TerraformTest{
+		Options: func(t *testing.T) *terraform.Options {
+			project := testutil.UniqueId()
+			env := testutil.UniqueId()
+			service := testutil.UniqueId()
+			owner := testutil.UniqueId()
 
-	bucketName := testutil.UniqueId()
+			bucketName := testutil.UniqueId()
 
-	options := testutil.Options(
-		testutil.DefaultRegion,
-		map[string]interface{}{
-			"project": project,
-			"env":     env,
-			"service": service,
-			"owner":   owner,
+			return testutil.Options(
+				testutil.DefaultRegion,
+				map[string]interface{}{
+					"project": project,
+					"env":     env,
+					"service": service,
+					"owner":   owner,
 
-			"bucket_name": bucketName,
+					"bucket_name": bucketName,
+				},
+			)
 		},
-	)
 
-	defer terraform.Destroy(t, options)
-	testutil.Run(t, options)
+		Validate: func(t *testing.T, options *terraform.Options) {
+			r := require.New(t)
+			region := options.EnvVars["AWS_DEFAULT_REGION"]
+			bucket := options.Vars["bucket_name"].(string)
+			aws.AssertS3BucketExists(t, region, bucket)
+			aws.AssertS3BucketPolicyExists(t, region, bucket)
+			aws.AssertS3BucketVersioningExists(t, region, bucket)
+			s3Client := aws.NewS3Client(t, region)
+
+			acl, err := s3Client.GetBucketAcl(&s3.GetBucketAclInput{
+				Bucket: &bucket,
+			})
+
+			r.NoError(err)
+			r.Len(acl.Grants, 1)
+
+			r.Equal("CanonicalUser", *acl.Grants[0].Grantee.Type)
+			r.Equal("FULL_CONTROL", *acl.Grants[0].Permission)
+
+			enc, err := s3Client.GetBucketEncryption(&s3.GetBucketEncryptionInput{
+				Bucket: &bucket,
+			})
+			r.NoError(err)
+
+			r.NotNil(enc.ServerSideEncryptionConfiguration)
+			r.Len(enc.ServerSideEncryptionConfiguration.Rules, 1)
+		},
+	}
+
+	test.Run(t)
+}
+
+type TerraformTest struct {
+	Options  func(*testing.T) *terraform.Options
+	Validate func(*testing.T, *terraform.Options)
+}
+
+func (tt *TerraformTest) Run(t *testing.T) {
+	terraformDirectory := "."
+
+	defer test_structure.RunTestStage(t, "cleanup", func() {
+		options := test_structure.LoadTerraformOptions(t, terraformDirectory)
+		terraform.Destroy(t, options)
+		testutil.Clean(terraformDirectory)
+		test_structure.CleanupTestDataFolder(t, terraformDirectory)
+	})
+
+	test_structure.RunTestStage(t, "options", func() {
+		options := tt.Options(t)
+		test_structure.SaveTerraformOptions(t, terraformDirectory, options)
+	})
+
+	test_structure.RunTestStage(t, "apply", func() {
+		options := test_structure.LoadTerraformOptions(t, terraformDirectory)
+		testutil.Run(t, options)
+	})
+
+	test_structure.RunTestStage(t, "validate", func() {
+		options := test_structure.LoadTerraformOptions(t, terraformDirectory)
+		tt.Validate(t, options)
+	})
 }
