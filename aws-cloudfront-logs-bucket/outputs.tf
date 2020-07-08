9cbd7bd1b3437b1c54b7d769343978bb725035ae
adoami <adoami@chanzuckerberg.com>
2020-07-07 21:48:09 -0700

    outputs

--- aws-cloudfront-logs-bucket/outputs.tf
+++ aws-cloudfront-logs-bucket/outputs.tf
@@ -1,16 +1,16 @@
 // HACK(el): we do this to hint TF dependency graph since modules can't depend_on
 output "name" {
-  value = module.aws-s3-private-bucket.name
+  value = module.aws-cloudfront-logs-bucket.name
 }
 
 output "domain_name" {
-  value = module.aws-s3-private-bucket.domain_name
+  value = module.aws-cloudfront-logs-bucket.domain_name
 }
 
 output "arn" {
-  value = module.aws-s3-private-bucket.arn
+  value = module.aws-cloudfront-logs-bucket.arn
 }
 
 output "id" {
-  value = module.aws-s3-private-bucket.id
+  value = module.aws-cloudfront-logs-bucket.id
 }
