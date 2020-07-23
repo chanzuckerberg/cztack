48d1962441bb2ed8060549e5ef9015418045eb4c
Ryan King <rking@chanzuckerberg.com>
2020-07-23 15:14:05 -0700

    fmt

--- aws-lambda-function/test/hello.py
+++ aws-lambda-function/test/hello.py
@@ -2,7 +2,12 @@
 def hello(event, context):
     message = 'Hello {}!'.format(event['name'])
     print(message)
-    return {"message": message, "log_group_name": context.log_group_name, "log_stream_name": context.log_stream_name, "aws_request_id": context.aws_request_id}
+    return {
+        "message": message,
+        "log_group_name": context.log_group_name,
+        "log_stream_name": context.log_stream_name,
+        "aws_request_id": context.aws_request_id
+    }
 
 
 if __name__ == '__main__':
