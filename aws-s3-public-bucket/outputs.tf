dafdaee1d7922e3aa3e45673590434cbd7594842
Annie <anne.ku@chanzuckerberg.com>
2020-12-01 15:34:32 -0800

    make fmt

--- aws-s3-public-bucket/outputs.tf
+++ aws-s3-public-bucket/outputs.tf
@@ -1,20 +1,20 @@
 // HACK(el): we do this to hint TF dependency graph since modules can't depend_on
 output "name" {
-  value = var.bucket_name
+  value       = var.bucket_name
   description = "Name of this public bucket. It should have `public` in its name"
 }
 
 output "domain_name" {
-  value = aws_s3_bucket.bucket.bucket_domain_name
+  value       = aws_s3_bucket.bucket.bucket_domain_name
   description = "domain where you host the public bucket"
 }
 
 output "arn" {
-  value = aws_s3_bucket.bucket.arn
+  value       = aws_s3_bucket.bucket.arn
   description = "Public bucket ARN"
 }
 
 output "id" {
-  value = aws_s3_bucket.bucket.id
+  value       = aws_s3_bucket.bucket.id
   description = "Public Bucket ID"
 }
