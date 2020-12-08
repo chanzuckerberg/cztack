e5e2f72eddea7cc752d853247a8e707f650570cb
Palash Agarwal <pagarwal@chanzuckerberg.com>
2020-12-07 17:37:43 -0800

    aws-cloudwatch-logs-retention-manager
    Create a scheduled lambda function to iterate through all Cloudwatch Log
    Groups and enforce a maximum retention. This is particularly useful for
    deletion efforts on the Education team, because Lambda@Edge functions
    (amongst others) can log PII, but have no way to control retention via
    Terraform.
    
    It's a good question whether we want this to be:
    - Opt-out: Applies to all log groups unless specified otherwise.
    - Opt-in: Only applies to specific prefixes.
    
    I think there's probably not much downside in the opt-out approach. It's
    the safest in terms of deletion requirements.
    
    Test Plan: Dry runs in the console with the `put_retention_policy`
    commented out.
    
    Pull Request Branch: palasha/aws_cloudwatch_logs_retention_manager
    Pull Request Link: https://github.com/chanzuckerberg/cztack/pull/263

---
+++ aws-cloudwatch-log-retention-manager/README.md
@@ -0,0 +1,42 @@
+# aws-cloudwatch-log-retention-manager
+
+This module schedules a lambda to do a daily scan through all cloudwatch logs groups, in all regions, and enforce a maximum retention policy. You can exempt individual log groups by setting the `ExemptFromMaxRetentionPolicy` tag to true.
+
+## Example
+
+```hcl
+module log-retention-audit {
+  source            = "github.com/chanzuckerberg/cztack/aws-cloudwatch-logs-retention-audit
+  default_retention = 30
+}
+```
+
+<!-- START -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| aws | < 3.0.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| archive | n/a |
+| aws | < 3.0.0 |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| env | Env for tagging and naming. See [doc](../README.md#consistent-tagging) | `string` | n/a | yes |
+| maximum\_retention | The default days of retention to apply to untagged Cloudwatch Log Groups. | `number` | n/a | yes |
+| owner | Owner for tagging and naming. See [doc](../README.md#consistent-tagging) | `string` | n/a | yes |
+| project | Project for tagging and naming. See [doc](../README.md#consistent-tagging) | `string` | n/a | yes |
+| service | Service for tagging and naming. See [doc](../README.md#consistent-tagging) | `string` | n/a | yes |
+
+## Outputs
+
+No output.
+
+<!-- END -->
---
+++ aws-cloudwatch-log-retention-manager/main.tf
@@ -0,0 +1,117 @@
+locals {
+  tags = {
+    managedBy = "terraform"
+    env       = var.env
+    project   = var.project
+    service   = var.service
+    owner     = var.owner
+  }
+
+  lambda_name = "${var.project}-${var.env}-${var.service}-cloudwatch-retention"
+}
+
+data archive_file lambda {
+  type        = "zip"
+  output_path = "${path.module}/build/lambda.zip"
+  source {
+    filename = "function.rb"
+    content  = <<-EOF
+      require 'aws-sdk-ec2'
+      require 'aws-sdk-cloudwatchlogs'
+      DEFAULT_MAX_RETENTION = ${var.maximum_retention}
+
+      def handler(event:, context:)
+        regions = Aws::EC2::Client.new.describe_regions.regions.map(&:region_name)
+        regions.each do |region|
+          puts "Processing Region: #{region}"
+          cloudwatch = Aws::CloudWatchLogs::Client.new(region: region)
+          cloudwatch.describe_log_groups.each do |log_group_batch|
+            log_group_batch.log_groups.each do |log_group|
+              name = log_group.log_group_name
+              retention = log_group.retention_in_days
+
+              print "Region: #{region}, LogGroup: #{name}, Retention: #{retention || 'None'}, "
+
+              # If retention is set, and less than the required maximum, skip.
+              if retention && retention <= DEFAULT_MAX_RETENTION
+                print "Result: In Compliance\n"
+                next
+              end
+
+              # Specific log groups may be exempt from the maximum retention policy.
+              tags = cloudwatch.list_tags_log_group(log_group_name: log_group.log_group_name).tags
+              exempt = tags["ExemptFromMaxRetentionPolicy"] == "true"
+              if exempt
+                print "Result: Exempt\n"
+                next
+              end
+
+              # Add or replace existing retention to match policy.
+              cloudwatch.put_retention_policy(
+                log_group_name: log_group.log_group_name,
+                retention_in_days: DEFAUT_MAX_RETENTION
+              )
+              print "Result: Fixed\n"
+            end
+          end
+        end
+      end
+    EOF
+  }
+}
+
+module lambda {
+  source = "../aws-lambda-function"
+
+  function_name    = local.lambda_name
+  filename         = data.archive_file.lambda.output_path
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+  handler          = "function.handler"
+  runtime          = "ruby2.7"
+
+  timeout               = 300
+  log_retention_in_days = var.maximum_retention
+
+  env     = var.env
+  owner   = var.owner
+  project = var.project
+  service = var.service
+}
+
+resource aws_cloudwatch_event_rule trigger {
+  name                = "${var.project}-${var.env}-${var.service}-retention-trigger"
+  schedule_expression = "rate(5 minutes)"
+  tags                = local.tags
+}
+
+resource aws_cloudwatch_event_target trigger {
+  rule = aws_cloudwatch_event_rule.trigger.id
+  arn  = module.lambda.arn
+}
+
+resource aws_lambda_permission permission {
+  statement_id  = "AllowScheduledLambdaExecution"
+  action        = "lambda:InvokeFunction"
+  function_name = local.lambda_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.trigger.arn
+}
+
+data aws_iam_policy_document policy {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeRegions",
+      "logs:DescribeLogGroups",
+      "logs:ListTagsLogGroup",
+      "logs:PutRetentionPolicy",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource aws_iam_role_policy policy {
+  name   = "allow-managing-log-groups"
+  role   = module.lambda.role_id
+  policy = data.aws_iam_policy_document.policy.json
+}
