1b53806cbe7022dc7ab15be9843542ba1401a96a
David Rissato Cruz <davidrc@gmail.com>
2021-01-08 10:45:39 -0800

    [feature] Add aws-redis-replication-group (#267)
    ### Summary
    This module is similar to [aws-redis-node](https://github.com/chanzuckerberg/cztack/tree/main/aws-redis-node), however it creates a Elasticache Redis cluster using a replication group.
    The motivation to create this module is because encryption options (in-transit and at rest) are only available when you create your cluster using a replication group.
    
    ### Test Plan
    - Manual tests (already applied to IDseq)
    - Unit tests
    
    ### References
    (Optional) Additional links to provide more context.

---
+++ aws-redis-replication-group/module_test.go
@@ -0,0 +1,60 @@
+package test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/chanzuckerberg/go-misc/tftest"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAWSRedisReplicationGroup(t *testing.T) {
+	test := tftest.Test{
+
+		Setup: func(t *testing.T) *terraform.Options {
+			privateSubnets := tftest.ListEnvVar("PRIVATE_SUBNETS")
+			log.Printf("subnets %#v\n", privateSubnets)
+			log.Printf("subnets %#v\n", os.Getenv("PRIVATE_SUBNETS"))
+			vpc := tftest.EnvVar(tftest.EnvVPCID)
+
+			sg := tftest.CreateSecurityGroup(t, tftest.DefaultRegion, vpc)
+
+			project := tftest.UniqueID()
+			env := tftest.UniqueID()
+			service := tftest.UniqueID()
+			owner := tftest.UniqueID()
+			replication_group_description := tftest.UniqueID()
+			transit_encryption_enabled := true
+			at_rest_encryption_enabled := true
+
+			az := fmt.Sprintf("%sa", tftest.DefaultRegion)
+
+			return tftest.Options(tftest.DefaultRegion,
+				map[string]interface{}{
+					"project": project,
+					"env":     env,
+					"service": service,
+					"owner":   owner,
+
+					"availability_zones":         []string{az},
+					"subnets":                    privateSubnets,
+					"ingress_security_group_ids": []string{sg},
+					"vpc_id":                     vpc,
+
+					"replication_group_description": replication_group_description,
+					"transit_encryption_enabled":    transit_encryption_enabled,
+					"at_rest_encryption_enabled":    at_rest_encryption_enabled,
+				},
+			)
+		},
+		Validate: func(t *testing.T, options *terraform.Options) {},
+
+		Cleanup: func(t *testing.T, options *terraform.Options) {
+			tftest.DeleteSecurityGroup(t, tftest.DefaultRegion, options.Vars["ingress_security_group_ids"].([]interface{})[0].(string))
+		},
+	}
+
+	test.Run(t)
+}
