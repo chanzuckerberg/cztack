201e10fc32413d22b33c4d21da31f077a76e259c
Annie <anne.ku@chanzuckerberg.com>
2021-06-29 14:59:59 -0700

    [feature] assume role policy module

---
+++ aws-assume-role-policy/outputs.tf
@@ -0,0 +1,4 @@
+output "json" {
+    value = data.aws_iam_policy_document.assume-role.json
+    description = "JSON version of assume-role policy"
+}
---
+++ aws-assume-role-policy/variables.tf
@@ -0,0 +1,21 @@
+variable "source_account_ids" {
+  type = list(string)
+  description = "The source AWS account IDs to establish a trust relationship. Ignored if empty or not provided."
+}
+
+variable "saml_idp_arns" {
+  type = list(string)
+  description = "The AWS SAML IDP arns to establish a trust relationship. Ignored if empty or not provided."
+}
+
+variable "oidc" {
+  type = list(object(
+    {
+      idp_arn : string,          # the AWS IAM IDP arn
+      client_ids : list(string), # a list of oidc client ids
+      provider : string          # your provider url, such as foo.okta.com
+    }
+  ))
+  default = []
+  description = "A list of AWS OIDC IDPs to establish a trust relationship for this role."
+}
