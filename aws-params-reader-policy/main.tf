1c88e0fb37740f55d21226cabff921a22827835c
Annie <anne.ku@chanzuckerberg.com>
2021-07-23 11:27:38 -0700

    tweak comment to include author

--- aws-params-reader-policy/main.tf
+++ aws-params-reader-policy/main.tf
@@ -29,14 +29,15 @@ data "aws_iam_policy_document" "policy" {
     actions   = ["kms:Decrypt"]
     resources = [data.aws_kms_alias.parameter_store_key.target_key_arn]
   }
-  # To use this reader policy with chamber, we need to give blanket ssm:DescribeParameters perms
+  # Note (aku): To use this reader policy with chamber, we need to give blanket ssm:DescribeParameters perms
   # https://github.com/segmentio/chamber/blob/53baa2b77bb1ed279bea346533d50f4fb1c01ed1/store/ssmstore.go#L214-L217
-  # This should be okay because this is a list option and the permission won't allow you to read the contents
-  # Otherwise, we'd get this error: The actions in your policy do not support resource-level permissions and require you to choose All resources
+  # This should be okay because this is a list option and the permission won't allow you to read the secret values
+  # Without this, we'd get this error: The actions in your policy do not support resource-level permissions and require you to choose All resources
   statement {
     sid = "ChamberSSMReadRequirement"
     actions   = ["ssm:DescribeParameters"]
     resources = ["arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:parameter/*"]
+    
   }
 }
 
