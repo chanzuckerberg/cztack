f35b8fd704768770cfd860343df94ce0c60fc6c6
Annie <anne.ku@chanzuckerberg.com>
2021-07-23 11:30:26 -0700

    whitespace changes

--- aws-params-reader-policy/main.tf
+++ aws-params-reader-policy/main.tf
@@ -37,7 +37,6 @@ data "aws_iam_policy_document" "policy" {
     sid = "ChamberSSMReadRequirement"
     actions   = ["ssm:DescribeParameters"]
     resources = ["arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:parameter/*"]
-    
   }
 }
 
