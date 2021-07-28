59a216ced5f66c93867871deaa9d5578595fc28b
Annie <anne.ku@chanzuckerberg.com>
2021-07-28 15:17:29 -0700

    lint

--- aws-assume-role-policy/main.tf
+++ aws-assume-role-policy/main.tf
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "assume-role" {
   }
 
   dynamic "statement" {
-    for_each =var.saml_idp_arns
+    for_each = var.saml_idp_arns
     content {
       principals {
         type        = "Federated"
