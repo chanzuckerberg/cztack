6781cc69a031a5f86c80c304c6f41761e1f98876
Annie <anne.ku@chanzuckerberg.com>
2021-07-28 15:27:30 -0700

    get rid of singular account ID

--- aws-assume-role-policy/main.tf
+++ aws-assume-role-policy/main.tf
@@ -1,17 +1,6 @@
 
 data "aws_iam_policy_document" "assume-role" {
   dynamic "statement" {
-    for_each = compact([var.source_account_id])
-    content {
-      principals {
-        type        = "AWS"
-        identifiers = ["arn:aws:iam::${statement.value}:root"]
-      }
-      actions = ["sts:AssumeRole", "sts:TagSession"]
-    }
-  }
-
-  dynamic "statement" {
     for_each = var.source_account_ids
     content {
       principals {
