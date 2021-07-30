09df022ec857c538fcb1a8db549eaec289b4e926
Annie <anne.ku@chanzuckerberg.com>
2021-07-29 18:10:30 -0700

    fix unhandled merge conflict

--- aws-iam-role-infraci/variables.tf
+++ aws-iam-role-infraci/variables.tf
@@ -12,30 +12,18 @@ variable "terraform_state_lock_dynamodb_arns" {
   description = "ARNs of the state file DynamoDB tables"
 }
 
-<<<<<<< HEAD
-=======
-variable "saml_idp_arn" {
-  type        = string
-  default     = ""
-  description = "The AWS SAML IDP arn to establish a trust relationship. Ignored if empty or not provided."
-}
-
->>>>>>> 8e5e5ca8d3c0c1f86613031c52f2302726e1a358
 variable "source_account_ids" {
   type        = set(string)
   default     = []
   description = "The source AWS account IDs to establish a trust relationship. Ignored if empty or not provided."
 }
 
-<<<<<<< HEAD
 variable "saml_idp_arns" {
   type        = set(string)
   default     = []
   description = "The AWS SAML IDP arns to establish a trust relationship. Ignored if empty or not provided."
 }
 
-=======
->>>>>>> 8e5e5ca8d3c0c1f86613031c52f2302726e1a358
 variable "project" {
   type        = string
   description = "Project for tagging and naming. See [doc](../README.md#consistent-tagging)"
