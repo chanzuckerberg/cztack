6c53aa1573769717f4b8856c1d4b270595f553ee
Annie <anne.ku@chanzuckerberg.com>
2021-08-04 19:11:08 -0700

    delete statement block

--- aws-assume-role-policy/main.tf
+++ aws-assume-role-policy/main.tf
@@ -65,20 +65,3 @@ data "aws_iam_policy_document" "assume-role" {
     }
   }
 }
-
-statement {
-  sid     = "ExternalPartyAssume"
-  effect  = "Allow"
-  actions = ["sts:AssumeRole"]
-
-  principals {
-    type        = "AWS"
-    identifiers = ["arn:aws:iam::${var.caller_account_id}:root"]
-  }
-
-  condition {
-    test     = "StringEquals"
-    variable = "sts:ExternalId"
-    values   = [var.sts_external_id]
-  }
-}
