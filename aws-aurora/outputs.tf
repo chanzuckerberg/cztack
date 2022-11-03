4409728098a4585b74a2a9295bf0dc0190807afc
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2022-11-03 14:31:51 -0400

    rename

--- aws-aurora/outputs.tf
+++ aws-aurora/outputs.tf
@@ -2,11 +2,11 @@ output "database_name" {
   value = aws_rds_cluster.db.database_name
 }
 
-output "master_username" {
+output "database_username" {
   value = var.database_username
 }
 
-output "master_password" {
+output "database_password" {
   value = var.database_password
 }
 
