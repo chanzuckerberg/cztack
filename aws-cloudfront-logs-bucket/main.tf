10d1e08da94dc8dd1516b0a2b53e9a2bb7cd5143
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-05-24 16:13:28 -0700

    debug.

--- aws-cloudfront-logs-bucket/main.tf
+++ aws-cloudfront-logs-bucket/main.tf
@@ -48,8 +48,8 @@ resource "aws_s3_bucket_ownership_controls" "example" {
 resource "aws_s3_bucket_public_access_block" "remove_public_access" {
   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
