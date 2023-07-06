2684a0fee3c62186d3512d00340e6264b9f312ab
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-07-06 11:18:28 -0700

    chore: remove deprecated attribute (#515)

--- aws-s3-private-bucket/main.tf
+++ aws-s3-private-bucket/main.tf
@@ -119,14 +119,17 @@ resource "aws_s3_bucket" "bucket" {
     }
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  tags = local.tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "enc" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
     }
   }
-  tags = local.tags
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
