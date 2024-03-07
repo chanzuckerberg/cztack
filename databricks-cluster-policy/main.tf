924fd873fd6cb75f5bcad97bf7bd6b21252dd2ec
Jason Ng <jng@chanzuckerberg.com>
2024-03-06 16:00:57 -0800

    Change for each to dynamic

--- databricks-cluster-policy/main.tf
+++ databricks-cluster-policy/main.tf
@@ -35,12 +35,14 @@ resource "databricks_cluster_policy" "inherited_cluster_policy" {
 }
 
 resource "databricks_permissions" "can_use_inherited_cluster_policy" {
-  for_each = local.inherited_cluster_policy_grantees
-
   cluster_policy_id = databricks_cluster_policy.inherited_cluster_policy[0].id
-  access_control {
-    group_name       = each.value
-    permission_level = "CAN_USE"
+  
+  dynamic "access_control" {
+    for_each = local.inherited_cluster_policy_grantees
+    content {
+      group_name       = access_control.value
+      permission_level = "CAN_USE"
+    }
   }
 }
 
@@ -53,11 +55,13 @@ resource "databricks_cluster_policy" "custom_cluster_policy" {
 }
 
 resource "databricks_permissions" "can_use_custom_cluster_policy" {
-  for_each = local.custom_cluster_policy_grantees
-
   cluster_policy_id = databricks_cluster_policy.custom_cluster_policy[0].id
-  access_control {
-    group_name       = each.value
-    permission_level = "CAN_USE"
+
+  dynamic "access_control" {
+    for_each = local.custom_cluster_policy_grantees
+    content {
+      group_name       = access_control.value
+      permission_level = "CAN_USE"
+    }
   }
 }
