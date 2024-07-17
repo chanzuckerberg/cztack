3660a34ee38bbb42373595070ba376cbdc9880bb
Jason Ng <jng@chanzuckerberg.com>
2024-07-17 16:05:04 -0700

    ref fix

--- databricks-s3-volume/outputs.tf
+++ databricks-s3-volume/outputs.tf
@@ -1,5 +1,5 @@
 output "dbx_unity_aws_role_arn" {
-  value = aws_iam_role.dbx_unity_aws_role[0].arn
+  value = length(aws_iam_role.dbx_unity_aws_role) > 0 ? aws_iam_role.dbx_unity_aws_role[0].arn : null
 }
 
 output "volume_specific_bucket_name" {
