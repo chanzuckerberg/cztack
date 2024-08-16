8b47a1cfc0708bbd7ef3a877821e8af3bb10ffcc
Allison Doami <adoami@chanzuckerberg.com>
2024-08-16 01:03:00 -0500

    feat: Create databricks catalog ext loc modules (#614)

---
+++ databricks-catalog-external-location/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "1.49.1"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
