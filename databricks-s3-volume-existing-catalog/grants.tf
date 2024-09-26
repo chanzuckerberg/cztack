4a55197550bfa5b66cb653b299cc96dd0523aa1c
Allison Doami <adoami@chanzuckerberg.com>
2024-09-26 16:46:19 -0700

    flatten

--- databricks-s3-volume-existing-catalog/grants.tf
+++ databricks-s3-volume-existing-catalog/grants.tf
@@ -1,19 +1,37 @@
+locals {
+  volume_r_grants = flatten([
+    for bucket in var.volume_buckets : [
+      for principal in bucket.volume_r_grant_principals : {
+        bucket_name = bucket.bucket_name
+        principal   = principal
+      }
+    ]
+  ])
+
+  volume_rw_grants = flatten([
+    for bucket in var.volume_buckets : [
+      for principal in bucket.volume_rw_grant_principals : {
+        bucket_name = bucket.bucket_name
+        principal   = principal
+      }
+    ]
+  ])
+}
+
+# Read-only access grants
 resource "databricks_grant" "volume_r" {
-  for_each = {
-    for bucket in var.volume_buckets : "${bucket.bucket_name}_${bucket.volume_r_grant_principals}" => bucket
-    if length(bucket.volume_r_grant_principals) > 0
-  }
-  volume    = each.value.bucket_name
-  principal = each.value.volume_r_grant_principals[count.index]
+  for_each = { for idx, grant in local.volume_r_grants : "${grant.bucket_name}-${grant.principal}" => grant }
+
+  volume     = each.value.bucket_name
+  principal  = each.value.principal
   privileges = ["READ_VOLUME"]
 }
 
+# Read/write access grants
 resource "databricks_grant" "volume_rw" {
-  for_each = {
-    for bucket in var.volume_buckets : "${bucket.bucket_name}_${bucket.volume_rw_grant_principals}" => bucket
-    if length(bucket.volume_rw_grant_principals) > 0
-  }
-  volume    = each.value.bucket_name
-  principal = each.value.volume_rw_grant_principals[count.index]
+  for_each = { for idx, grant in local.volume_rw_grants : "${grant.bucket_name}-${grant.principal}" => grant }
+
+  volume     = each.value.bucket_name
+  principal  = each.value.principal
   privileges = ["READ_VOLUME", "WRITE_VOLUME"]
 }
