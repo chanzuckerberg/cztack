d0e004f658dd2d17a9b61dec296cfd0875f329c9
Allison Doami <adoami@chanzuckerberg.com>
2024-10-17 13:37:45 -0700

    fix: Move out power user group

--- databricks-default-cluster-policies/variables.tf
+++ databricks-default-cluster-policies/variables.tf
@@ -34,3 +34,8 @@ variable "policy_map" {
   description = "Map of policy names to groups"
   type = list(map(list(string)))
 }
+
+variable "power_user_group_name" {
+  description = "Name of the power user group"
+  type = string
+}
