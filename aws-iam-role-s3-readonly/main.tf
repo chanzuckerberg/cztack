f78c19ef7578e5f626ebe35e80ec159dc30cddc1
Vivian Shao <nshao@chanzuckerberg.com>
2024-10-31 14:33:24 -0700

    feat: biohub s3 readonly role

---
+++ aws-iam-role-s3-readonly/main.tf
@@ -0,0 +1,67 @@
+locals {
+  tags = {
+    env     = var.env
+    owner   = var.owner
+    service = var.service
+    project = var.project
+  }
+}
+
+module "assume_role_policy" {
+  source             = "../aws-assume-role-policy"
+  source_account_ids = var.source_account_ids
+  saml_idp_arns      = var.saml_idp_arns
+  env                = var.env
+  owner              = var.owner
+  service            = var.service
+  project            = var.project
+}
+
+resource "aws_iam_role" "role" {
+  name               = var.role_name
+  assume_role_policy = module.assume_role_policy.json
+  tags               = local.tags
+}
+
+data "aws_iam_policy_document" "s3-bucket-readonly" {
+  statement {
+    sid    = "GetFiles"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersion"
+    ]
+    resources = formatlist("arn:aws:s3:::%s*/*", var.s3_bucket_prefixes)
+  }
+  statement {
+    sid    = "ListBucket"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads"
+    ]
+    resources = formatlist("arn:aws:s3:::%s*/*", var.s3_bucket_prefixes)
+  }
+  statement {
+    sid    = "ShowAllowedBuckets"
+    effect = "Allow"
+    actions = [
+      "s3:ListAllMyBuckets"
+    ]
+    resources = toset(formatlist("arn:aws:s3:::%s", var.s3_bucket_prefixes), formatlist("arn:aws:s3:::%s/*", var.s3_bucket_prefixes))
+  }
+}
+
+resource "aws_iam_policy" "s3" {
+  name        = "${var.role_name}-s3"
+  description = "Provide access to s3 resources for a distribution ${var.role_name}"
+  policy      = data.aws_iam_policy_document.s3-bucket-readonly.json
+}
+
+resource "aws_iam_role_policy_attachment" "s3" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.s3.arn
+}
---
+++ aws-iam-role-s3-readonly/variables.tf
@@ -0,0 +1,50 @@
+variable "role_name" {
+  type        = string
+  description = "Name of the role to create"
+}
+
+variable "s3_bucket_prefixes" {
+  type        = list(any)
+  description = "Limits role permissions to buckets with specific prefixes. Empty for all buckets."
+
+  default = [
+    "",
+  ]
+}
+
+variable "iam_path" {
+  type    = string
+  default = "/"
+}
+
+variable "source_account_ids" {
+  type        = set(string)
+  default     = []
+  description = "The source AWS account IDs to establish a trust relationship. Ignored if empty or not provided."
+}
+
+variable "saml_idp_arns" {
+  type        = set(string)
+  default     = []
+  description = "The AWS SAML IDP arns to establish a trust relationship. Ignored if empty or not provided."
+}
+
+variable "project" {
+  type        = string
+  description = "Project for tagging and naming. See [doc](../README.md#consistent-tagging)"
+}
+
+variable "env" {
+  type        = string
+  description = "Env for tagging and naming. See [doc](../README.md#consistent-tagging)"
+}
+
+variable "service" {
+  type        = string
+  description = "Service for tagging and naming. See [doc](../README.md#consistent-tagging)"
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner for tagging and naming. See [doc](../README.md#consistent-tagging)"
+}
