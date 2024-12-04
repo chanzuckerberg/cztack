0ef712218bf805abebdffc5a98670312abe73e92
Jason Ng <jng@chanzuckerberg.com>
2024-12-04 12:34:29 -0800

    fix: remove non-applicable permission from dbx volume grant

--- databricks-s3-volume/grants.tf
+++ databricks-s3-volume/grants.tf
@@ -64,7 +64,7 @@ resource "databricks_grant" "volume_r" {
   for_each   = toset(var.volume_r_grant_principals)
   volume     = databricks_volume.volume.id
   principal  = each.value
-  privileges = ["READ_VOLUME", "READ_FILES"]
+  privileges = ["READ_VOLUME"]
 }
 
 resource "databricks_grant" "volume_rw" {
