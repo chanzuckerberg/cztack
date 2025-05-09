bc0a0dac32afdba95f80e840fc6898df007fa2c3
James Bartolome <jbartolome@chanzuckerberg.com>
2025-05-09 12:36:41 -0700

    map to bucket value

--- databricks-s3-volume-existing-catalog/grants.tf
+++ databricks-s3-volume-existing-catalog/grants.tf
@@ -1,11 +1,11 @@
 resource "databricks_grants" "volume" {
-  for_each = toset([for bucket in var.volume_buckets : bucket.volume_name])
+  for_each = toset([for bucket in var.volume_buckets : bucket.volume_name => bucket])
 
   volume = each.value
 
   # Read-only access grants
   dynamic "grant" {
-    for_each = each.value.volume_r_grant_principals
+    for_each = toset(each.value.volume_r_grant_principals)
     content {
       principal = grant.value
       privileges = ["READ_VOLUME"]
@@ -14,7 +14,7 @@ resource "databricks_grants" "volume" {
 
   # Read/write access grants
   dynamic "grant" {
-    for_each = each.value.volume_rw_grant_principals
+    for_each = toset(each.value.volume_rw_grant_principals)
     content {
       principal = grant.value
       privileges = ["READ_VOLUME", "WRITE_VOLUME"]
