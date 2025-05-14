f12fd8f2269d9073f4af6dcde601c78b725073d6
James Bartolome <jbartolome@chanzuckerberg.com>
2025-05-14 13:55:51 -0700

    fix: Assign all listed Databricks usergroups to associated Databricks cluster policies, refactor

--- databricks-default-cluster-policies/groups.tf
+++ databricks-default-cluster-policies/groups.tf
@@ -3,44 +3,33 @@ locals {
     var.policy_name_prefix,
   ])
 
-  # Define the policies with existing groups mapped to each policy
-  policies = var.policy_map
-
-  # Generate full workspace policy names by prefixing policy names
-  all_ws_policy_names = flatten([
-    for prefix in local.ws_policy_name_prefixes : [
-      for policy_map in local.policies :
-      "${prefix}${keys(policy_map)[0]}"
-    ]
+  policy_group_membership_list = toset([
+    for prefix in local.ws_policy_name_prefixes
+    : for policy_suffix, groups_names in var.policy_map
+      : for group_name in groups_names
+        : "${prefix}${policy_suffix}" => "${group_name}"
   ])
-
-  # Create a flat map of policy names to associated groups
-  policy_group_map = merge([for policy_map in local.policies : policy_map]...)
 }
 
 # Create Databricks groups for each policy name
 resource "databricks_group" "ws_policy_groups" {
-  for_each = toset(local.all_ws_policy_names)
+  for_each = toset(keys(local.policy_group_membership_list))
 
   display_name     = each.key
   workspace_access = true
 }
 
 # Retrieve the existing Databricks groups that need to be assigned
-data "databricks_group" "groups" {
-  for_each = toset(flatten([
-    for group in local.policy_group_map : group
-  ]))
+data "databricks_group" "usergroups" {
+  for_each = toset(values(local.policy_group_membership_list))
 
   display_name = each.value
 }
 
-# Assign the existing groups to the newly created policy groups
+# Assign user groups to policy groups per policy-usergroup pair
 resource "databricks_group_member" "ws_policy_group_members" {
-  for_each = databricks_group.ws_policy_groups
+  for_each = local.policy_group_membership_list
 
-  group_id = each.value.id
-
-  # Assign all existing groups that correspond to this policy group
-  member_id = data.databricks_group.groups[local.policy_group_map[replace(each.key, var.policy_name_prefix, "")][0]].id
+  group_id = data.databricks_group.ws_policy_groups[each.key]
+  member_id = data.databricks_group.groups[each.value].id
 }
