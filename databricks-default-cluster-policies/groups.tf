1dea31cbd0f089a2c558f776eb186f2d7c0500fb
James Bartolome <jbartolome@chanzuckerberg.com>
2025-05-14 14:11:32 -0700

    fix iterator

--- databricks-default-cluster-policies/groups.tf
+++ databricks-default-cluster-policies/groups.tf
@@ -5,7 +5,7 @@ locals {
 
   policy_group_membership_list = toset([
     for prefix in local.ws_policy_name_prefixes
-    : [ for policy_suffix, groups_names in var.policy_map
+    : [ for policy_suffix, groups_names in merge(var.policy_map...)
       : [ for group_name in groups_names
         : "${prefix}${policy_suffix}" => "${group_name}"
       ]
