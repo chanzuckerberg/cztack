11bb3aad6c90bf6309be925be472e9fadc0a5d4e
James Bartolome <jbartolome@chanzuckerberg.com>
2025-05-14 17:14:55 -0700

    refactor

--- databricks-default-cluster-policies/groups.tf
+++ databricks-default-cluster-policies/groups.tf
@@ -3,20 +3,30 @@ locals {
     var.policy_name_prefix,
   ])
 
-  policy_group_membership_list = toset(flatten([
-    for prefix in local.ws_policy_name_prefixes
-    : [ for policy_suffix, groups_names in merge(var.policy_map...)
-      : [ for group_name in groups_names
-        : "${prefix} - ${policy_suffix} - ${group_name}" = {
-            policy : "${prefix}${policy_suffix}",
-            group : group_name
+  #policy_group_membership_list = toset(flatten([
+  #  for prefix in local.ws_policy_name_prefixes
+  #  : [ for policy_suffix, groups_names in merge(var.policy_map...)
+  #    : [ for group_name in groups_names
+  #      : "${prefix} - ${policy_suffix} - ${group_name}" = {
+  #          policy : "${prefix}${policy_suffix}",
+  #          group : group_name
+  #      }
+  #    ]
+  #  ]
+  #]))
+  policy_group_memberships = flatten([
+    for prefix in local.ws_policy_name_prefixes : [
+      for policy_suffix, groups_names in merge(var.policy_map...) : [
+        for group_name in groups_names : {
+          policy = "${prefix}${policy_suffix}",
+          group  = group_name
         }
       ]
     ]
-  ]))
+  ])
 
-  ws_cluster_policy_names = toset(flatten([for _, pair in local.policy_group_membership_list : pair.policy]))
-  usergroups_names = toset(flatten([for _, pair in local.policy_group_membership_list : pair.group]))
+  ws_cluster_policy_names = flatten([for pair in local.policy_group_memberships : pair.policy])
+  usergroups_names        = flatten([for pair in local.policy_group_memberships : pair.group])
 }
 
 # Create Databricks groups for each policy name
@@ -37,13 +47,13 @@ data "databricks_group" "usergroups" {
 # Assign user groups to policy groups per policy-usergroup pair
 resource "databricks_group_member" "ws_policy_group_members" {
   for_each = {
-    for _, pair in local.policy_group_membership_list
-    : _ => {
-      policy: pair.policy,
-      group: pair.group,
+    for pair in local.policy_group_memberships :
+    "${pair.policy}+${group}" => {
+      policy : pair.policy,
+      group : pair.group,
     }
   }
 
-  group_id = databricks_group.ws_cluster_policy_groups[each.value.policy].id
+  group_id  = databricks_group.ws_cluster_policy_groups[each.value.policy].id
   member_id = data.databricks_group.usergroups[each.value.group].id
 }
--- databricks-default-cluster-policies/outputs.tf
+++ databricks-default-cluster-policies/outputs.tf
@@ -1,3 +1,3 @@
-output policy_group_membership_list {
+output "policy_group_membership_map" {
   value = local.policy_group_membership_list
 }
