e79fea832d6ee9199b800dd5bfc12adf2fd7ea1a
jjb007 <jbartolome@chanzuckerberg.com>
2025-05-21 15:46:54 -0700

    fix syntax

--- databricks-s3-volume-existing-catalog/main.tf
+++ databricks-s3-volume-existing-catalog/main.tf
@@ -19,7 +19,7 @@ locals {
           bucket.bucket_aws_account_id,
           data.aws_caller_identity.current.account_id,
         ),
-        bucket_access_role_name = replace("${var.catalog_name}-${bucket.volume_name}-vol-ax-role"),
+        bucket_access_role_name = replace("${var.catalog_name}-${bucket.volume_name}-vol-ax-role", "-", "_"),
         bucket_access_role_arn = join(
           ":", [
             "arn",
@@ -27,7 +27,7 @@ locals {
             "iam",
             "",
             bucket.bucket_aws_account_id,
-            join("/", ["role", "databricks", bucket.bucket_access_role_name], "-", "_")
+            join("/", ["role", "databricks", bucket.bucket_access_role_name])
           ]
         ),
         bucket_arn = join(
