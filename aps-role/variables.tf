e0f3fa44a4bd683fce654d3c44e13efaa4f481f3
Allison Doami <adoami@chanzuckerberg.com>
2025-08-18 15:22:31 -0700

    chore: Remove aps role module

--- aps-role/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {
-  description = "The name of the EKS cluster"
-  type        = string
-}
-
-variable "tags" {
-  type        = object({ project : string, env : string, service : string, owner : string, managedBy : string })
-  description = "Tags to apply to resources"
-}
