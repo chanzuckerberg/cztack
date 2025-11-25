b3541b68da23f1953d6261ba7d0675a9352e8570
Jason Ng <jng@chanzuckerberg.com>
2025-11-24 19:19:29 -0800

    Missed other file

--- databricks-catalog-external-location/versions.tf
+++ databricks-catalog-external-location/versions.tf
@@ -6,6 +6,7 @@ terraform {
     databricks = {
       source  = "databricks/databricks"
       version = "1.49.1"
+      configuration_aliases = [ databricks.mws, databricks.workspace ]
     }
   }
   required_version = ">= 1.3.0"
